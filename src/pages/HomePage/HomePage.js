55924a2034bd195006618f1b295ebd4c95bcc2e0
Pavlo Kozlov <kozlov.pavel.s@gmail.com>
2021-05-22 10:16:27 +0200

    Add empty state

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import Input from '../../components/Input';
 import ResultsList from '../../components/ResultsList';
+import './HomePage.scss';
 
 const HomePage = ({ searchValue, onSearchChange, results }) => {
   return (
-    <div>
-      <Input value={searchValue} onChange={onSearchChange} placeholder="Type something..."></Input>
+    <div className="home-page">
+      <Input value={searchValue} onChange={onSearchChange} placeholder="Type here..."></Input>
       {/* <Button onClick={() => console.log('click')}>Search</Button> */}
-      <ResultsList values={results}></ResultsList>
+      <div className="home-page-results__container">
+        {
+          searchValue.length > 0 ?
+            <ResultsList values={results}></ResultsList> :
+            <span className="home-page-results__empty">Type something in</span>
+        }
+      </div>
     </div>
   );
 }
